04c510008020ea8f59eb92a2efd61448470d9de7
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-16 12:45:12 -0600

    Finish the cleanUp file

--- cleanUp.js
+++ cleanUp.js
@@ -4,41 +4,46 @@
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
   // make sure the code you submit WORKS - you can run this entire file 
-    // using `node cleanUp.js` or copy and paste into a replit
+  // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName() { 
+  console.log("Hello, what is your name? ");
+  // var name = prompt("Hello, what is your name? ");
+  // console.log("Hello " + name + " it's nice to meet you!");
+}
 
-askForName()
+askForName();
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+  var sum = first + second + third
+  return sum;
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return avg
+}
 
-return avg
-  }
+console.log(average(3, 5));
