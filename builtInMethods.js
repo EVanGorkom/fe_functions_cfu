3b4c8e3d1a401fce3790cbb0aa812fd17e454bca
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-16 13:00:03 -0600

    Finish the builtInMethods file

--- builtInMethods.js
+++ builtInMethods.js
@@ -8,13 +8,13 @@
 // The return value is "hello world"
 "Hello World".toLowerCase();
 
-
+// This checks to see if the argument "Hello" is present in the object before it. It will return a boolean.
 "Hello World".includes("Hello");
 
-
+// This checks to see if the object ends with the value equivalent to "Hello". It will return a boolean.
 "Hello World".endsWith("Hello");
 
-
+// As above.
 "Hello World".endsWith("rld");
 
 
@@ -28,15 +28,26 @@
 // EXAMPLE
 // The startsWith() method is called on the firstName variable, which stores the string object "Jeff". 
 // The startsWith() method returns true if the data in the firstName variable starts with the argument passed in.
-// In this example, the return value is true, because "Jeff" does start with "J".
 // The console.log() statements prints the return value of the startsWith() method (true) to the console.
-var firstName = "Jeff";
-console.log(firstName.startsWith("J"));
-
-
+var firstName = "Ethan";
+console.log(firstName.charAt(0));
+var catName = "Vader";
+console.log(catName.length);
 
 // SECTION 3: Calling methods on variables assigned to arrays.
 // Declare 2 variables assigned to arrays.
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+var philosophers = ["Aristotle", "Plato", "Socrates"];
+var books = ["Nicomachean Ethics", "The Republic", "5 Dialogues"]
+
+for (i = 0; i < philosophers.length; i++) {
+  console.log(philosophers[i])
+}
+
+for (i = 0; i < books.length; i++) {
+  if (books[i].startsWith("Nico")) {
+    console.log(books[i]);
+  }
+}
