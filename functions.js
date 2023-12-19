66523260c8494110d9df6beec236db14028dde98
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-18 20:57:58 -0600

    Finish the 'functions' file

--- functions.js
+++ functions.js
@@ -4,20 +4,46 @@
 // If a function takes parameters, call the function at least twice, passing different arguments each time.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
-
+// function greeting() {
+//   console.log("Hello there!");
+// }
+// greeting();
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-
+// function customGreeting(name) {
+//   console.log("Hello there " + name + "!");
+// }
+// customGreeting("David");
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+// function greetPerson(firstName, middleName, lastName) {
+//   console.log("Hello " + firstName + " " + middleName + " " + lastName + " it's a pleasure to meet you!");
+// }
+// greetPerson("Alistair", "Marion", "Crawley");
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
-
+// function square(num) {
+//   var square_value = num * num;
+//   console.log("The square of " + num + " is equal to " + square_value);
+// }
+// square(1);
+// square(2);
+// square(3);
+// square(4);
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
+
+// function checkStock(stock_num, name) {
+//   if (stock_num <= 0) {
+//     console.log(name + " - OUT of stock!");
+//   } else if (stock_num <= 3) {
+//     console.log(name + " - running LOW!");
+//   } else {
+//     console.log(name + " is stocked.");
+//   }
+// }
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
